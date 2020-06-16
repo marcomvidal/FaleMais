8441107bd91ef388bd962824c298e06748943b7b
Marco M. Vidal <marcomvidal@gmail.com>
2020-06-16 16:58:18 -0300

    Calculation view structure

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import { postalCodes, plans } from '../data/data';
+import { areaCodes, plans } from '../data/data';
+import { Card } from '../components/Card';
+import { InputContainer } from '../components/InputContainer';
+import { SimulationResult } from '../models/SimulationResult';
+import { SimulationResultCard } from '../components/SimulationResultCard';
+import { Simulation } from '../models/Simulation';
 
 export class Calculator extends Component {
   state = {
-    postalCodes: postalCodes,
-    plans: plans
+    codes: areaCodes,
+    plans: plans,
+    source: areaCodes[0],
+    destination: areaCodes[1],
+    callTime: 1,
+    selectedPlan: plans[0],
+    result: null
   };
 
-  postalCodesSelection() {
-    return (
-      <select className="form-control">
-        {this.state.postalCodes.map(postalCode => <option>{postalCode.number}</option>)}
-      </select>
-    );
+  handleSource = event => this.setState({ source: this.state.codes[event.target.value] });
+  
+  handleDestination = event => this.setState({ destination: this.state.codes[event.target.value] });
+
+  handleCallTime = event => this.setState({ callTime: event.target.value });
+
+  handlePlan = event => this.setState({ selectedPlan: this.state.plans[event.target.value] });
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const simulation = new Simulation(this.state.selectedPlan, this.state.callTime);
+    this.setState({ result: simulation.generate() });
   }
 
-  plansSelection() {
-    return (
-      <select className="form-control">
-        {this.state.plans.map(plan => <option>{plan.brand}</option>)}
-      </select>
-    );
+  codesOptions() {
+    return this.state.codes.map(code => <option key={code.id} value={code.id}>{code.number}</option>);
   }
 
   render() {
@@ -28,30 +40,32 @@ export class Calculator extends Component {
       <React.Fragment>
         <h1>Calculadora</h1>
 
-        <div className="card">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-3">
-                <label>DDD de origem:</label>
-                {this.postalCodesSelection()}
-              </div>
-              <div className="col-md-3">
-                <label>DDD de destino:</label>
-                {this.postalCodesSelection()}
-              </div>
-              <div className="col-md-3">
-                <label>Tempo da ligação:</label>
-                <input className="form-control" type="number"></input>
-              </div>
-              <div className="col-md-3">
-                <label>Plano:</label>
-                {this.plansSelection()}
-              </div>
-            </div>
-          </div>
-        
-          
-        </div>
+        <Card header="Simulação">
+          <form className="row" onSubmit={this.handleSubmit}>
+            <InputContainer label="DDD de origem:" className="col-md-3">
+              <select className="form-control" value={this.state.source.id} onChange={this.handleSource}>
+                {this.codesOptions()}
+              </select>
+            </InputContainer>
+            <InputContainer label="DDD de destino:" className="col-md-3">
+              <select className="form-control" value={this.state.destination.id} onChange={this.handleDestination}>
+                {this.codesOptions()}
+              </select>
+            </InputContainer>
+            <InputContainer label="Minutos de ligação:" className="col-md-3">
+              <input className="form-control" type="number" value={this.state.callTime} onChange={this.handleCallTime} />
+            </InputContainer>
+            <InputContainer label="Plano" className="col-md-3">
+              <select className="form-control" value={this.state.selectedPlan.id} onChange={this.handlePlan}>
+                {this.state.plans.map(plan => <option key={plan.id} value={plan.id}>{plan.brand}</option>)}
+              </select>
+            </InputContainer>
+
+            <button type="submit" className="btn btn-primary offset-md-5 col-md-2">Simular</button>
+          </form>
+        </Card>
+
+        <SimulationResultCard result={this.state.result} plan={this.state.selectedPlan} />
       </React.Fragment>
     );
   }
